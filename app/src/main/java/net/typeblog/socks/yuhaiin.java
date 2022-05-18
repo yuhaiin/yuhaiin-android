25e523b0d20096afdf5be02e8ca2df074520de48
asutorufa <16442314+Asutorufa@users.noreply.github.com>
2022-05-18 16:58:23 +0800

    add fakedns

--- app/src/main/java/net/typeblog/socks/yuhaiin.java
+++ app/src/main/java/net/typeblog/socks/yuhaiin.java
@@ -6,18 +6,24 @@ public class yuhaiin extends Thread {
     private final String path;
     private final String dnsServer;
     private final String socks5Server;
+    private final String httpserver;
+    private final String fakedns;
+    private final boolean fakednse;
     private final Yuhaiin_ yuhaiin = new Yuhaiin_();
-    yuhaiin(String host,String path,String dnsServer,String socks5Server){
+    yuhaiin(String host,String path,String dnsServer,String socks5Server,String httpserver,String fakedns,boolean fakednse){
         this.host = host;
         this.path = path;
         this.dnsServer = dnsServer;
         this.socks5Server = socks5Server;
+        this.httpserver =httpserver;
+        this.fakednse =fakednse;
+        this.fakedns = fakedns;
     }
 
     @Override
     public void run() {
         try {
-            yuhaiin.start(this.host,this.path,this.dnsServer,this.socks5Server);
+            yuhaiin.start(this.host,this.path,this.dnsServer,this.socks5Server,this.httpserver, this.fakednse,this.fakedns);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
