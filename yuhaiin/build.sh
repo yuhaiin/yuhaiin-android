349fb417780f1b3cf0f1458ce2054001c8cb2f22
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2023-04-15 13:45:56 +0800

    remove useless authentication switch and edittext

--- yuhaiin/build.sh
+++ yuhaiin/build.sh
@@ -1,12 +1,12 @@
-current=$(dirname $(readlink -f "$0"))
+current=$(dirname "$(readlink -f "$0")")
 
 # android ndk 自带clang有问题
 # 直接是个txt里面写的clang-xx 然后任何参数都没传
 # 需要手动将toolchains/llvm/prebuilt/linux-x86_64/bin/clang-xx软链接到clang和clang++
 if [ -d "${HOME}/Documents/Programming/yuhaiin" ]; then
-  cd ${HOME}/Documents/Programming/yuhaiin
-elif [ -d "/mnt/shareSSD/code/yuhaiin" ];then
-  cd /mnt/shareSSD/code/yuhaiin
+  cd "${HOME}"/Documents/Programming/yuhaiin || exit 1
+elif [ -d "/mnt/shareSSD/code/yuhaiin" ]; then
+  cd /mnt/shareSSD/code/yuhaiin || exit 1
 else
   echo "can't find yuhaiin dir"
   exit 1
@@ -16,14 +16,18 @@ if [ -d "${HOME}/.local/storage/Android/Sdk/" ]; then
   export ANDROID_HOME="${HOME}/.local/storage/Android/Sdk/"
 fi
 
-
 if [ -d "${HOME}/.local/storage/Android/android-ndk-r23c" ]; then
   export ANDROID_NDK_HOME="${HOME}/.local/storage/Android/android-ndk-r23c"
 fi
 
-java="${HOME}/.local/share/JetBrains/Toolbox/apps/AndroidStudio/ch-0/223.8836.35.2231.9848316/jbr/bin/"
-if [ -d ${java} ]; then
-  export PATH=$PATH:${java}
+java="${HOME}/.local/share/JetBrains/Toolbox/apps/AndroidStudio/ch-0"
+if [ -d "${java}" ]; then
+  for file in "${java}"/*; do
+    if [ -f "${file}/jbr/bin/javac" ]; then
+      export PATH=$PATH:"${file}/jbr/bin"
+      break
+    fi
+  done
 fi
 
-GOPROXY=https://goproxy.cn,direct gomobile bind -ldflags='-s -w -buildid=' -trimpath -target="android/arm64,android/amd64" -o ${current}/yuhaiin.aar -v ./cmd/android/
+GOPROXY=https://goproxy.cn,direct gomobile bind -ldflags='-s -w -buildid=' -trimpath -target="android/arm64,android/amd64" -o "${current}"/yuhaiin.aar -v ./cmd/android/
