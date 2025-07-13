33793d637edd47de31e95c9347a2885652aa95cd
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2025-07-13 13:38:14 +0800

    generate tun address to bind

--- app/src/main/kotlin/io/github/asutorufa/yuhaiin/MainApplication.kt
+++ app/src/main/kotlin/io/github/asutorufa/yuhaiin/MainApplication.kt
@@ -1,26 +1,106 @@
 package io.github.asutorufa.yuhaiin
 
 import android.app.Application
+import android.net.ConnectivityManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.content.getSystemService
 import com.google.android.material.color.DynamicColors
 import go.Seq
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import yuhaiin.Interfaces
 import yuhaiin.Store
 import yuhaiin.Yuhaiin
+import java.net.InetSocketAddress
+import java.net.NetworkInterface
+import java.util.Locale
 
 open class MainApplication : Application() {
 
     companion object {
         lateinit var store: Store
+
     }
+
+    val connectivity by lazy { this.getSystemService<ConnectivityManager>()!! }
+
+    inner class UidDumper : yuhaiin.UidDumper {
+        @RequiresApi(Build.VERSION_CODES.Q)
+        override fun dumpUid(
+            p0: Int,
+            srcIp: String?,
+            srcPort: Int,
+            destIp: String?,
+            destPort: Int
+        ): Int =
+            connectivity.getConnectionOwnerUid(
+                p0,
+                InetSocketAddress(srcIp, srcPort),
+                InetSocketAddress(destIp, destPort)
+            )
+
+        override fun getUidInfo(p0: Int): String = packageManager.getNameForUid(p0) ?: "unknown"
+    }
+
+    private val uidDumper = UidDumper()
 
     override fun onCreate() {
         super.onCreate()
 
         Seq.setContext(this)
-        Yuhaiin.initDB(getExternalFilesDir("yuhaiin").toString(),applicationInfo.dataDir)
+
+        Yuhaiin.setSavePath(getExternalFilesDir("yuhaiin").toString())
+        Yuhaiin.setDataDir(applicationInfo.dataDir)
+        Yuhaiin.setInterfaces(GetInterfaces())
+        Yuhaiin.setProcessDumper(uidDumper)
         store = Yuhaiin.getStore()
         DynamicColors.applyToActivitiesIfAvailable(this)
+    }
+
+    inner class GetInterfaces : Interfaces {
+        override fun getInterfacesAsString(): String? {
+            val interfaces: ArrayList<NetworkInterface> =
+                java.util.Collections.list(NetworkInterface.getNetworkInterfaces())
+
+            val sb = StringBuilder()
+            for (nif in interfaces) {
+                try {
+                    sb.append(
+                        String.format(
+                            Locale.ROOT,
+                            "%s %d %d %b %b %b %b %b |",
+                            nif.name,
+                            nif.index,
+                            nif.mtu,
+                            nif.isUp,
+                            nif.supportsMulticast(),
+                            nif.isLoopback,
+                            nif.isPointToPoint,
+                            nif.supportsMulticast()
+                        )
+                    )
+
+                    for (ia in nif.interfaceAddresses) {
+                        val parts = ia.toString().split("/", limit = 0)
+                        if (parts.size > 1) {
+                            sb.append(
+                                String.format(
+                                    Locale.ROOT,
+                                    "%s/%d ",
+                                    parts[1],
+                                    ia.networkPrefixLength
+                                )
+                            )
+                        }
+                    }
+                } catch (e: Exception) {
+                    continue
+                }
+                sb.append("\n")
+            }
+
+            return sb.toString()
+        }
     }
 }
 
