63a4246f441511b959f6e86a91cbf296ed320221
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2025-07-17 01:58:43 +0800

    update GetInterfaces

--- app/src/main/kotlin/io/github/asutorufa/yuhaiin/MainApplication.kt
+++ app/src/main/kotlin/io/github/asutorufa/yuhaiin/MainApplication.kt
@@ -8,12 +8,15 @@ import androidx.core.content.getSystemService
 import com.google.android.material.color.DynamicColors
 import go.Seq
 import kotlinx.serialization.json.Json
+import yuhaiin.AddressIter
+import yuhaiin.AddressPrefix
+import yuhaiin.Interface
+import yuhaiin.InterfaceIter
 import yuhaiin.Interfaces
 import yuhaiin.Store
 import yuhaiin.Yuhaiin
 import java.net.InetSocketAddress
 import java.net.NetworkInterface
-import java.util.Locale
 
 open class MainApplication : Application() {
 
@@ -57,49 +60,86 @@ open class MainApplication : Application() {
         DynamicColors.applyToActivitiesIfAvailable(this)
     }
 
+    class InterfaceIterImpl(
+        private val data: ArrayList<Interface> // 或 MutableList<Interface>
+    ) : InterfaceIter {
+        private var index = 0
+
+        override fun next(): Interface? {
+            return if (index < data.size) {
+                data[index++]
+            } else {
+                null
+            }
+        }
+
+        override fun hasNext(): Boolean {
+            return index < data.size
+        }
+
+        override fun reset() {
+            index = 0
+        }
+    }
+
+    class AddressIterImpl(
+        private val data: ArrayList<AddressPrefix> // 或 MutableList<Interface>
+    ) : AddressIter {
+        private var index = 0
+
+        override fun next(): AddressPrefix? {
+            return if (index < data.size) {
+                data[index++]
+            } else {
+                null
+            }
+        }
+
+        override fun hasNext(): Boolean {
+            return index < data.size
+        }
+
+        override fun reset() {
+            index = 0
+        }
+    }
+
     inner class GetInterfaces : Interfaces {
-        override fun getInterfacesAsString(): String? {
+        override fun getInterfaces(): InterfaceIter? {
             val interfaces: ArrayList<NetworkInterface> =
                 java.util.Collections.list(NetworkInterface.getNetworkInterfaces())
-
-            val sb = StringBuilder()
+            val sb = ArrayList<Interface>(0)
             for (nif in interfaces) {
-                try {
-                    sb.append(
-                        String.format(
-                            Locale.ROOT,
-                            "%s %d %d %b %b %b %b %b |",
-                            nif.name,
-                            nif.index,
-                            nif.mtu,
-                            nif.isUp,
-                            nif.supportsMulticast(),
-                            nif.isLoopback,
-                            nif.isPointToPoint,
-                            nif.supportsMulticast()
-                        )
-                    )
 
-                    for (ia in nif.interfaceAddresses) {
-                        val parts = ia.toString().split("/", limit = 0)
-                        if (parts.size > 1) {
-                            sb.append(
-                                String.format(
-                                    Locale.ROOT,
-                                    "%s/%d ",
-                                    parts[1],
-                                    ia.networkPrefixLength
-                                )
-                            )
-                        }
-                    }
+                try {
+                    sb.add(Interface().apply {
+                        name = nif.name
+                        displayName = nif.displayName
+                        index = nif.index
+                        mtu = nif.mtu
+                        isVirtual = nif.isVirtual
+                        hardwareAddr = nif.hardwareAddress
+                        isUp = nif.isUp
+                        broadcast = nif.supportsMulticast()
+                        isLoopback = nif.isLoopback
+                        isPointToPoint = nif.isPointToPoint
+                        supportsMulticast = nif.supportsMulticast()
+                        address = AddressIterImpl(ArrayList<AddressPrefix>().apply {
+                            for (ia in nif.interfaceAddresses) {
+                                add(AddressPrefix().apply {
+                                    address = ia.address.toString().trimStart('/')
+                                    mask = ia.networkPrefixLength.toInt()
+                                    broadcast = ia.broadcast?.toString()?.trimStart('/')
+                                })
+                            }
+                        })
+                    })
                 } catch (e: Exception) {
                     continue
                 }
-                sb.append("\n")
             }
 
-            return sb.toString()
+            return InterfaceIterImpl(sb)
         }
     }
 }
