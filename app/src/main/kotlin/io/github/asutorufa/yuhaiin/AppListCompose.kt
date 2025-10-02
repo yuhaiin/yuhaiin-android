bdeb0641b5679dca53f90165571431013fcfc3c8
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2025-10-02 11:25:09 +0800

    add filter to app list

--- app/src/main/kotlin/io/github/asutorufa/yuhaiin/AppListCompose.kt
+++ app/src/main/kotlin/io/github/asutorufa/yuhaiin/AppListCompose.kt
@@ -17,14 +17,32 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.AppBarWithSearch
+import androidx.compose.material3.AppBarWithSearchColors
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.LoadingIndicator
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBarColors
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.material3.SearchBarValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberSearchBarState
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
@@ -35,11 +53,14 @@ import androidx.compose.runtime.snapshots.SnapshotStateSet
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.graphics.drawable.toBitmap
 import androidx.core.graphics.drawable.toDrawable
+import androidx.navigation.NavController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -52,9 +73,9 @@ data class AppListData(
 )
 
 @SuppressLint("ViewModelConstructorInComposable")
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@OptIn(ExperimentalMaterial3ExpressiveApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun AppListComponent(packageManager: PackageManager) {
+fun AppListComponent(navController: NavController, packageManager: PackageManager) {
     val checkedApps = remember {
         mutableStateSetOf(
             *MainApplication.store.getStringSet("app_list").toTypedArray()
@@ -91,20 +112,75 @@ fun AppListComponent(packageManager: PackageManager) {
         }
     }
 
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .systemBarsPadding()
-    ) {
-        if (data == null) {
-            LoadingIndicator(
-                modifier = Modifier
-                    .align(Alignment.Center)
-                    .size(55.dp)
+    val textFieldState = rememberTextFieldState()
+    val searchBarState = rememberSearchBarState()
+    val scrollBehavior = SearchBarDefaults.enterAlwaysSearchBarScrollBehavior()
+
+    val inputField =
+        @Composable {
+            SearchBarDefaults.InputField(
+                modifier = Modifier,
+                searchBarState = searchBarState,
+                textFieldState = textFieldState,
+                onSearch = {},
+                placeholder = {
+                    if (searchBarState.currentValue == SearchBarValue.Collapsed) {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "Filter",
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                },
+                leadingIcon = {
+                    Icon(Icons.Default.Search, contentDescription = null)
+                },
             )
-        } else
-            AppList(apps = data!!, checkedApps = checkedApps)
-    }
+        }
+
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            AppBarWithSearch(
+                state = searchBarState,
+                scrollBehavior = scrollBehavior,
+                inputField = inputField,
+                navigationIcon = {
+                    TooltipBox(
+                        positionProvider =
+                            TooltipDefaults.rememberTooltipPositionProvider(
+                                TooltipAnchorPosition.Above
+                            ),
+                        tooltip = { PlainTooltip { Text("Back") } },
+                        state = rememberTooltipState(),
+                    ) {
+                        IconButton(onClick = { navController.popBackStack() }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                                contentDescription = "Back"
+                            )
+                        }
+                    }
+                }
+            )
+        },
+        content = { padding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {
+                if (data == null) {
+                    LoadingIndicator(
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .size(55.dp)
+                    )
+                } else
+                    AppList(apps = data!!, checkedApps = checkedApps, filter = textFieldState.text)
+            }
+        }
+    )
 }
 
 @Composable
@@ -126,10 +202,21 @@ fun AppList(
             false
         )
     ),
+    filter: CharSequence? = null
 ) {
 
     LazyColumn(modifier = modifier) {
         items(apps) { app ->
+            if (filter != null &&
+                filter != "" &&
+                !app.packageName.contains(filter) &&
+                !app.appName.contains(
+                    filter
+                )
+            ) {
+                return@items
+            }
+
             AppListItem(app, onClick = {
                 if (checkedApps.contains(app.packageName)) checkedApps.remove(app.packageName)
                 else checkedApps.add(app.packageName)
