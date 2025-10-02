63f96b5e811ad0e0c03da20aa932a5393416f144
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2025-10-02 17:43:43 +0800

    change scope type

--- logcatviewer/src/main/kotlin/com/github/logviewer/LogcatActivity.kt
+++ logcatviewer/src/main/kotlin/com/github/logviewer/LogcatActivity.kt
@@ -11,12 +11,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.Date
 import java.util.regex.Pattern
 
@@ -24,31 +25,25 @@ import java.util.regex.Pattern
 class LogcatActivity : AppCompatActivity() {
     private val excludeList by lazy { intent.getStringArrayListExtra(INTENT_EXCLUDE_LIST)!! }
 
-    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val logs = remember { mutableStateListOf<LogEntry>() }
             var stop by rememberSaveable { mutableStateOf(false) }
-            start(stop) { logs.add(it) }
-            DisposableEffect(Unit) {
-                onDispose {
-                    stop = true
-                }
-            }
+            val scope = rememberCoroutineScope()
+            start(stop, scope) { logs.add(it) }
+            DisposableEffect(Unit) { onDispose { stop = true } }
 
             LogcatScreen(logs = logs)
         }
     }
-
 
     private fun skip(mExcludeList: List<Pattern>, line: String): Boolean {
         for (pattern in mExcludeList) if (pattern.matcher(line).matches()) return true
         return false
     }
 
-    @DelicateCoroutinesApi
-    fun start(stop: Boolean, pushLogs: (LogEntry) -> Unit) {
+    fun start(stop: Boolean, scope: CoroutineScope, pushLogs: (LogEntry) -> Unit) {
         val mExcludeList: MutableList<Pattern> = ArrayList()
         excludeList.forEach {
             try {
@@ -58,29 +53,30 @@ class LogcatActivity : AppCompatActivity() {
             }
         }
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val cmd = ArrayList(listOf("logcat", "-v", "threadtime"))
+        scope.launch {
+            withContext(Dispatchers.IO) {
+                val cmd = ArrayList(listOf("logcat", "-v", "threadtime"))
 
-            val process = ProcessBuilder(cmd).start()
+                val process = ProcessBuilder(cmd).start()
 
-            process.inputStream.bufferedReader().use {
-                while (!stop)
-                    it.readLine()?.let { line ->
-                        if (skip(mExcludeList, line))
-                            return@let
-                        try {
+                process.inputStream.bufferedReader().use {
+                    while (!stop)
+                        it.readLine()?.let { line ->
+                            if (skip(mExcludeList, line))
+                                return@let
+                            try {
 
-                            pushLogs(parseLog(line))
-                        } catch (e: Exception) {
-                            Log.w("parse log failed", "$e: log: $line")
-                            pushLogs(LogEntry(LogLevel.INFO, Date().toString(), line))
-                        }
-                    } ?: break
+                                pushLogs(parseLog(line))
+                            } catch (e: Exception) {
+                                Log.w("parse log failed", "$e: log: $line")
+                                pushLogs(LogEntry(LogLevel.INFO, Date().toString(), line))
+                            }
+                        } ?: break
+                }
+                process.destroy()
             }
-            process.destroy()
         }
     }
-
 
     companion object {
         const val INTENT_EXCLUDE_LIST = "exclude_list"
