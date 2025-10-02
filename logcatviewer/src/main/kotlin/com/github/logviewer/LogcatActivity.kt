5a4f0f69200a83c31537a96413570331b8909a4e
Asutorufa <16442314+Asutorufa@users.noreply.github.com>
2025-10-02 18:07:06 +0800

    optimize logcat reader

--- logcatviewer/src/main/kotlin/com/github/logviewer/LogcatActivity.kt
+++ logcatviewer/src/main/kotlin/com/github/logviewer/LogcatActivity.kt
@@ -7,33 +7,42 @@ import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import java.util.Date
 import java.util.regex.Pattern
 
 
 class LogcatActivity : AppCompatActivity() {
-    private val excludeList by lazy { intent.getStringArrayListExtra(INTENT_EXCLUDE_LIST)!! }
+    private val excludeList by lazy {
+        val mExcludeList: MutableList<Pattern> = ArrayList()
+        intent.getStringArrayListExtra(INTENT_EXCLUDE_LIST)!!.forEach {
+            try {
+                mExcludeList.add(Pattern.compile(it))
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+        return@lazy mExcludeList
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             val logs = remember { mutableStateListOf<LogEntry>() }
-            var stop by rememberSaveable { mutableStateOf(false) }
             val scope = rememberCoroutineScope()
-            start(stop, scope) { logs.add(it) }
-            DisposableEffect(Unit) { onDispose { stop = true } }
-
+            val process = runLogcat(scope) { logs.add(it) }
+            DisposableEffect(Unit) {
+                onDispose {
+                    logs.add(LogEntry(LogLevel.INFO, "", "stop read logcat"))
+                    process.destroy()
+                }
+            }
             LogcatScreen(logs = logs)
         }
     }
@@ -43,48 +52,37 @@ class LogcatActivity : AppCompatActivity() {
         return false
     }
 
-    fun start(stop: Boolean, scope: CoroutineScope, pushLogs: (LogEntry) -> Unit) {
-        val mExcludeList: MutableList<Pattern> = ArrayList()
-        excludeList.forEach {
-            try {
-                mExcludeList.add(Pattern.compile(it))
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }
+    fun runLogcat(scope: CoroutineScope, pushLogs: (LogEntry) -> Unit): Process {
+        val process = ProcessBuilder(listOf("logcat", "-v", "threadtime")).start()
 
-        scope.launch {
-            withContext(Dispatchers.IO) {
-                val cmd = ArrayList(listOf("logcat", "-v", "threadtime"))
-
-                val process = ProcessBuilder(cmd).start()
-
-                process.inputStream.bufferedReader().use {
-                    while (!stop)
-                        it.readLine()?.let { line ->
-                            if (skip(mExcludeList, line))
-                                return@let
-                            try {
-
-                                pushLogs(parseLog(line))
-                            } catch (e: Exception) {
-                                Log.w("parse log failed", "$e: log: $line")
-                                pushLogs(LogEntry(LogLevel.INFO, Date().toString(), line))
-                            }
-                        } ?: break
+        scope.launch(Dispatchers.IO) {
+            process.inputStream.bufferedReader().use {
+                while (true) {
+                    it.readLine()?.let { line ->
+                        if (skip(excludeList, line))
+                            return@let
+                        try {
+                            pushLogs(parseLog(line))
+                        } catch (e: Exception) {
+                            Log.w("parse log failed", "$e: log: $line")
+                            pushLogs(LogEntry(LogLevel.INFO, Date().toString(), line))
+                        }
+                    } ?: break
                 }
-                process.destroy()
             }
+
+            Log.i("logcat process", "stop read logcat")
+            process.destroy()
         }
+
+        return process
     }
 
     companion object {
         const val INTENT_EXCLUDE_LIST = "exclude_list"
-        fun start(excludeList: ArrayList<String>, activity: Activity) {
-            val starter = Intent(activity, LogcatActivity::class.java)
-                .putStringArrayListExtra(INTENT_EXCLUDE_LIST, excludeList)
-            activity.startActivity(starter)
-        }
+
+        fun start(excludeList: ArrayList<String>, activity: Activity) =
+            activity.startActivity(intent(excludeList, activity))
 
         fun intent(excludeList: ArrayList<String>, activity: Activity) =
             Intent(activity, LogcatActivity::class.java)
